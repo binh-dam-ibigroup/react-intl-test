e0afed14773cd16e290b643fcbe0b0c835aa9c4e
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2021-03-22 11:13:17 -0400

    Add basic i18n scenario.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { defineMessages, FormattedDate, FormattedMessage, FormattedNumber, FormattedTime, IntlProvider } from "react-intl";
+
+
+const messages = {
+  depart: {
+    id: "depart",
+    //defaultMessage: "TransitFare",
+    description: "Label for itinerary departure in narrative.",
+    values: {departDate: new Date()}
+  },
+  transitFare: {
+    id: "transitFare",
+    //defaultMessage: "TransitFare",
+    description: "Label for transit fare summary in narrative.",
+    values: {fare: 1.50}
+  }
+};
+
+const translatedMessages = {
+  "en-US": {
+    depart: "Depart {departDate, date, ::yyyyMMMMdd} at {departDate, time, ::hh:mm}",
+    transitFare: "Transit fare: {fare, number, ::.00 currency/USD}",
+    walkDistance: "Walk {dist, number, ::unit/mile}"
+  },
+  "fr": {
+    depart: "Départ le {departDate, date, ::yyyyMMMMdd} à {departDate, time, ::HH:mm}",
+    transitFare: "Coût en transports : {fare, number, ::.00 currency/EUR}",
+    walkDistance: "{dist, number, ::unit/kilometer} de marche"
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {["en-US", "fr"].map(locale => (
+        <IntlProvider locale={locale} messages={translatedMessages[locale]} defaultLocale='en-US'>
+          <ul>
+            <li>{locale}</li>
+            <li><FormattedMessage {...messages.depart} /></li>
+            <li><FormattedMessage {...messages.transitFare} /></li>
+            <li><FormattedMessage id='walkDistance' values={{dist: 0.4}} /></li>
+          </ul>
+        </IntlProvider>
+        ))}
       </header>
     </div>
   );
